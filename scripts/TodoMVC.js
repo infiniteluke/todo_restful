923102fa8a94aa779651e3c18250f982593816c8
infiniteluke <lherrington100@gmail.com>
2015-05-14 10:33:41 -0700

    Built client from commit 7de40b4 on branch master

---
+++ scripts/TodoMVC.js
@@ -0,0 +1,27 @@
+/*global Backbone */
+'use strict';
+
+// TodoMVC is global for developing in the console
+// and functional testing.
+var App = Backbone.Marionette.Application.extend({
+  setRootLayout: function() {
+    this.root = new TodoMVC.Layout.Root();
+  }
+});
+
+window.ENV = 'http://live-todo-restful.pantheon.io/';
+window.TodoMVC = new App();
+
+(function () {
+  var filterState = new Backbone.Model({
+    filter: 'all'
+  });
+
+  TodoMVC.reqres.setHandler('filterState', function () {
+    return filterState;
+  });
+})();
+
+TodoMVC.on('before:start', function () {
+  TodoMVC.setRootLayout();
+});
